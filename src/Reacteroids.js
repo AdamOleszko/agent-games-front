c186dfdae897c1d35a5fded68fc920f9f410f914
Piotr Badura <bajdurato@gmail.com>
2019-05-25 04:15:43 +0200

    asteroidy

--- src/Reacteroids.js
+++ src/Reacteroids.js
@@ -280,10 +280,9 @@ export class Reacteroids extends Component {
       <div>
         { endgame }
         <span className="score current-score" >Score: {this.state.currentScore}</span>
-        <span className="score top-score" >Top Score: {this.state.topScore}</span>
+        {/* <span className="score top-score" >Top Score: {this.state.topScore}</span> */}
         <span className="controls" >
-          Use [A][S][W][D] or [←][↑][↓][→] to MOVE<br/>
-          Use [SPACE] to SHOOT
+          Use mouse to move <br/> Press space to pause
         </span>
         <canvas id="gameBoard" ref="canvas"
           width={this.state.screen.width * this.state.screen.ratio}
