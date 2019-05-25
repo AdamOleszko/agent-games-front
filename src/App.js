59db710a4751e0d28b43dd87d792f09a6fd5faf3
Adam Oleszko <adamoleszko44@gmail.com>
2019-05-25 03:19:27 +0200

    livechat-api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { accountsSdk } from '@livechat/accounts-sdk';
+import config from './config.js';
 
-function App() {
+const App = () => {
+  // const client_id = 'b9a9ce57595f6814655643cbf9ddaa96';
+  // const redirectUrl = 'http://localhost:3000';
+
+  const [accessToken, setAccessToken] = useState(null);
+
+  useEffect(() => {
+    const { client_id, account_url } = config;
+    accountsSdk.init({
+      client_id,
+      onIdentityFetched: (error, data) => {
+        if (data && data.access_token) {
+          console.warn(accessToken);
+          setAccessToken(data.access_token);
+        } else {
+          window.location.href = `${account_url}?response_type=token&client_id=${client_id}&redirect_uri=${
+            window.location.href
+          }`;
+        }
+      }
+    });
+  });
+
+  const handleClick = async () => {
+    const test = await axios.get(`${config.server_url}/agents/`, {headers: {'X-API-Version': '2', Authorization: `Bearer ${accessToken}`}});
+    console.log(test);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {handleClick()}}>Download from livechat API</button>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const config = {
+  server_url:
+    "https://us-central1-livechat-experiments.cloudfunctions.net/restApi",
+    // server_url:'api.livechatinc.com',
+  account_url: "https://accounts.livechatinc.com/",
+  client_id: 'b9a9ce57595f6814655643cbf9ddaa96'
+}
+
+export default config;
